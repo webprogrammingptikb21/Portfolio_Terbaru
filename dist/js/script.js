8ea70433fc48d5e56f5ed1a03c3fa6ef4488318d
webprogrammingptikb21 <alwi3479@gmail.com>
2023-10-29 09:16:11 +0800

    menambahkan fitur baru di taiwlind

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixNav = header.offsetTop;
+  const toTop = document.querySelector("#toTop");
 
   if (window.pageYOffset > fixNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -16,4 +21,12 @@ const navbarMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navbarMenu.classList.toggle("hidden");
+});
+
+// klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navbarMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navbarMenu.classList.add("hidden");
+  }
 });
